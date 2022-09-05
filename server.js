1af31a74fcafe8bf148b56c15fb6d1e722479381
Atul Jha <mail.atuljha@gmail.com>
2022-09-05 18:22:54 +0530

    Intro to Middleware

--- server.js
+++ server.js
@@ -1,11 +1,18 @@
 const express = require("express");
 const dotenv = require("dotenv");
+const logger = require("./middleware/logger");
+
+//Route Files
 const bootcamps = require("./routes/bootcamp");
 
+//Load Env variables
 dotenv.config({ path: "./config/config.env" });
 
 const app = express();
 
+app.use(logger);
+
+//Mount Routers
 app.use("/api/vi/bootcamp", bootcamps);
 
 const PORT = process.env.PORT || 5000;
