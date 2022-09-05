15cad9a5afc946406632e8c652b4470424b94748
Atul Jha <mail.atuljha@gmail.com>
2022-09-05 17:37:42 +0530

    Using express router

--- server.js
+++ server.js
@@ -1,35 +1,12 @@
 const express = require("express");
 const dotenv = require("dotenv");
+const bootcamps = require("./routes/bootcamp");
 
 dotenv.config({ path: "./config/config.env" });
 
 const app = express();
 
-app.get("/api/vi/bootcamp", (req, res) => {
-  res.status(200).json({ success: true, msg: `Show Bootcamp` });
-});
-
-app.get("/api/vi/bootcamp/:id", (req, res) => {
-  res
-    .status(200)
-    .json({ success: true, msg: `Show Bootcamp ${req.params.id}` });
-});
-
-app.post("/api/vi/bootcamp", (req, res) => {
-  res.status(200).json({ success: true, msg: `Create Bootcamp` });
-});
-
-app.put("/api/vi/bootcamp/:id", (req, res) => {
-  res
-    .status(200)
-    .json({ success: true, msg: `Update Bootcamp ${req.params.id}` });
-});
-
-app.delete("/api/vi/bootcamp/:id", (req, res) => {
-  res
-    .status(200)
-    .json({ success: true, msg: `Delete Bootcamp ${req.params.id}` });
-});
+app.use("/api/vi/bootcamp", bootcamps);
 
 const PORT = process.env.PORT || 5000;
 
