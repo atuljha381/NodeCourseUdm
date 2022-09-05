81aea305914112eb467df8840d0af7e62f1012ea
Atul Jha <mail.atuljha@gmail.com>
2022-09-05 17:16:23 +0530

    Created Routers of express

--- server.js
+++ server.js
@@ -5,6 +5,32 @@ dotenv.config({ path: "./config/config.env" });
 
 const app = express();
 
+app.get("/api/vi/bootcamp", (req, res) => {
+  res.status(200).json({ success: true, msg: `Show Bootcamp` });
+});
+
+app.get("/api/vi/bootcamp/:id", (req, res) => {
+  res
+    .status(200)
+    .json({ success: true, msg: `Show Bootcamp ${req.params.id}` });
+});
+
+app.post("/api/vi/bootcamp", (req, res) => {
+  res.status(200).json({ success: true, msg: `Create Bootcamp` });
+});
+
+app.put("/api/vi/bootcamp/:id", (req, res) => {
+  res
+    .status(200)
+    .json({ success: true, msg: `Update Bootcamp ${req.params.id}` });
+});
+
+app.delete("/api/vi/bootcamp/:id", (req, res) => {
+  res
+    .status(200)
+    .json({ success: true, msg: `Delete Bootcamp ${req.params.id}` });
+});
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(
