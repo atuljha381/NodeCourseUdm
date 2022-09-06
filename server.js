b638890d16b61883d8e31e7beb116ca4a4b34292
atuljha381 <mail.atuljha@gmail.com>
2022-09-06 17:35:14 +0530

    Error handler middleware

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const express = require("express");
 const color = require("colors");
 const logger = require("./middleware/logger");
 const connectDB = require("./Config/db");
+const errorHandler = require("./middleware/error");
 
 //Load Env variables
 dotenv.config({ path: "./Config/config.env" });
@@ -16,19 +17,22 @@ const bootcamps = require("./routes/bootcamp");
 const app = express();
 
 //Body Parser
-app.use(express.json())
+app.use(express.json());
 
 app.use(logger);
 
 //Mount Routers
 app.use("/api/v1/bootcamp", bootcamps);
 
+app.use(errorHandler);
+
 const PORT = process.env.PORT;
 
 const server = app.listen(
   PORT,
   console.log(
-    `Server running on ${process.env.NODE_ENV} on port ${PORT}`.cyan.bold.underline
+    `Server running on ${process.env.NODE_ENV} on port ${PORT}`.cyan.bold
+      .underline
   )
 );
 
