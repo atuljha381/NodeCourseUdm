84e056e54459fb8fee77d98b90251d1fdbb34743
Atul Jha <atuljha@pop-os.localdomain>
2022-09-06 12:14:54 +0530

    MongoDB Connected with Error Handling

--- server.js
+++ server.js
@@ -1,23 +1,34 @@
-const express = require("express");
 const dotenv = require("dotenv");
+const express = require("express");
 const logger = require("./middleware/logger");
+const connectDB = require("./Config/db");
+
+//Load Env variables
+dotenv.config({ path: "./Config/config.env" });
+
+//Connect database
+connectDB();
 
 //Route Files
 const bootcamps = require("./routes/bootcamp");
-
-//Load Env variables
-dotenv.config({ path: "./config/config.env" });
 
 const app = express();
 
 app.use(logger);
 
 //Mount Routers
-app.use("/api/vi/bootcamp", bootcamps);
+app.use("/api/v1/bootcamp", bootcamps);
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT;
 
-app.listen(
+const server = app.listen(
   PORT,
   console.log(`Server running on ${process.env.NODE_ENV} on port ${PORT}`)
 );
+
+//Handle unhandled promise rejection
+process.on("unhandledRejection", (err, promise) => {
+  console.log(`Error: ${err.message}`);
+  //Close server and exit process
+  server.close(() => process.exit(1));
+});
